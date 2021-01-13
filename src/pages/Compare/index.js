538e81066995f25727d8dff5580465633587e537
fabianaColombo <fabicriscolombo@gmail.com>
2021-01-13 20:35:48 +0100

    Adding basic/initial UI to compare page - created input fields to explore youtube ids (using bootstrap) - imported graph component - button and graph are not functional yet

--- src/pages/Compare/index.js
+++ src/pages/Compare/index.js
@@ -1,5 +1,36 @@
 import React from "react";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import GraphSubscribersCount from "../../components/GraphSubscribersCount";
 
 export default function Compare() {
-  return <div>Compare Page</div>;
+  return (
+    <>
+      <Container>
+        <Form>
+          <Row>
+            <Col>
+              <Form.Control placeholder="YouTube ID" />
+            </Col>
+            vs
+            <Col>
+              <Form.Control placeholder="YouTube ID" />
+            </Col>
+            vs
+            <Col>
+              <Form.Control placeholder="YouTube ID" />
+            </Col>
+            <Button variant="danger">Explore!</Button>
+          </Row>
+        </Form>
+      </Container>
+
+      <Container width="90%" className="mt-5">
+        <GraphSubscribersCount />
+      </Container>
+    </>
+  );
 }
