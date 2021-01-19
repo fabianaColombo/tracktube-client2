351ab4c82169fb4863ad402181fc3d449775dde2
fabianaColombo <fabicriscolombo@gmail.com>
2021-01-19 15:37:00 +0100

    changing constants for api url in production

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,6 @@
-export const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:4000";
+export const apiUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://my-heroku-backend-or-whatever-you-call-it.herokuapp.com"
+    : "http://localhost:4000";
 
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
