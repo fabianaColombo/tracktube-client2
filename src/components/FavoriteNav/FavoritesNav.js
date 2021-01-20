91b4e88088da799873f9817ed1e4b3713892879d
fabianaColombo <fabicriscolombo@gmail.com>
2021-01-20 18:32:23 +0100

    Style v1 of Stats page using css and saas

--- src/components/FavoriteNav/FavoritesNav.js
+++ src/components/FavoriteNav/FavoritesNav.js
@@ -1,23 +1,21 @@
 import React from "react";
-import "./FavoriteNav.css";
+import "./FavoriteNav.scss";
 
 export default function FavoritesNav(props) {
-  return props.channels.map((channel) => {
-    console.log("props channel", props.channel);
-    return (
-      <div key={channel.youtubeId} className="d-inline-flex p-5 m-2">
-        <div
-          onClick={() => props.selectChannel(channel)}
-          className="card shadow-sm mb-4 border"
-        >
-          <div className="card-body pb-0 text-center ">
-            <h4 className="card-title mb-3">{channel.name}</h4>
-            <p className="card-subtitle mb-3 text-primary">
-              <a href="https://www.youtube.com/channel/">Visit on YouTube</a>
-            </p>
+  return (
+    <div className="favorite-nav-container">
+      {props.channels.map((channel) => {
+        console.log("props channel", props.channel);
+        return (
+          <div
+            key={channel.youtubeId}
+            onClick={() => props.selectChannel(channel)}
+            className="favorite-nav-card"
+          >
+            <h4>{channel.name}</h4>
           </div>
-        </div>
-      </div>
-    );
-  });
+        );
+      })}
+    </div>
+  );
 }
--- src/components/GlobalStats/GlobalStats.js
+++ src/components/GlobalStats/GlobalStats.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./GlobalStats.css";
+import "./GlobalStats.scss";
 
 export default function GlobalStats(props) {
   const channel = props.channel;
@@ -19,60 +19,83 @@ export default function GlobalStats(props) {
     );
   }
 
-  const averageViewsPerVideo = channel.totalViews / channel.videoUploads;
+  const averageViewsPerVideo = parseInt(
+    channel.totalViews / channel.videoUploads
+  );
 
-  const averageViewsPerSubscriber =
-    channel.totalViews / channel.subscriberCount;
+  const averageViewsPerSubscriber = parseInt(
+    channel.totalViews / channel.subscriberCount
+  );
 
   const today = new Date();
   const dateCreated = Date.parse(channel.created);
   const daysFromToday = (today - dateCreated) / (1000 * 3600 * 24);
 
-  const averageAccountGrowthPerDay = channel.subscriberCount / daysFromToday;
+  const averageAccountGrowthPerDay = parseInt(
+    channel.subscriberCount / daysFromToday
+  );
   //console.log(averageAccountGrowthPerDay);
+
+  function numberFormater(num) {
+    if (num > 999 && num < 1000000) {
+      return Math.sign(num) * (Math.abs(num) / 1000).toFixed(1) + "K";
+    } else if (num > 999999 && num < 1000000000) {
+      return Math.sign(num) * (Math.abs(num) / 1000000).toFixed(1) + "M";
+    } else if (num > 999999999) {
+      return Math.sign(num) * (Math.abs(num) / 1000000000).toFixed(1) + "B";
+    } else {
+      return num;
+    }
+  }
+  console.log(numberFormater(channel.subscriberCount));
 
   return (
     <div>
-      <div className="statsChannelTitle">
-        <div>
-          <h2>{channel.name}</h2>
-          <p>
-            Published At: <strong>{channel.created}</strong>
-          </p>
-        </div>
+      <div className="title">
+        <h2>
+          <a
+            target="_blank"
+            href={`https://www.youtube.com/channel/${channel.youtubeId}`}
+          >
+            {channel.name}
+          </a>
+        </h2>
+        <p>
+          Published At: <strong>{channel.created}</strong>
+        </p>
       </div>
-      <div>
-        <div className="globalStats"></div>
-        <div>
-          <h3>{channel.country}</h3>
+
+      <div className="stats-conatiner">
+        <div className="rounded-circle">
+          <h3>{numberFormater(channel.country)}</h3>
           <p>Country</p>
         </div>
-        <div>
-          <h3>{channel.subscriberCount}</h3>
+        <div className="rounded-circle">
+          <h3>{numberFormater(channel.subscriberCount)}</h3>
           <p>Subscribers</p>
         </div>
-        <div>
-          <h3>{channel.totalViews}</h3>
-          <p>Total Views</p>
+        <div className="rounded-circle">
+          <h3>{numberFormater(channel.totalViews)}</h3>
+          <p>Views</p>
         </div>
-        <div>
+        <div className="rounded-circle">
           <h3>{channel.videoUploads}</h3>
-          <p>Total Videos</p>
+          <p>Videos</p>
         </div>
+      </div>
 
-        <div>
-          <div>
-            <h3>{averageViewsPerVideo}</h3>
-            <p>Average Views Per Video</p>
-          </div>
-          <div>
-            <h3>{averageViewsPerSubscriber}</h3>
-            <p>Average Views Per Subscriber</p>
-          </div>
-          <div>
-            <h3>{averageAccountGrowthPerDay}</h3>
-            <p>Average Account Growth Per Day</p>
-          </div>
+      <div className="average-container">
+        <div className="average-box">
+          <h3>{numberFormater(averageViewsPerVideo)}</h3>
+          <p>Average Views Per Video</p>
+        </div>
+        <div className="average-box">
+          <h3>{numberFormater(averageViewsPerSubscriber)}</h3>
+          <p>Average Views Per Subscriber</p>
+        </div>
+        <div className="average-box">
+          <h3>{numberFormater(averageAccountGrowthPerDay)}</h3>
+          <p>Average Account Growth Per Day</p>
         </div>
       </div>
     </div>
