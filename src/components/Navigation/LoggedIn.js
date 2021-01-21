4a66add5e43b20b1b03b629eee832f7bb79b069e
fabianaColombo <fabicriscolombo@gmail.com>
2021-01-21 20:48:47 +0100

    adding explainer/marketing video on homepage

--- src/components/Navigation/LoggedIn.js
+++ src/components/Navigation/LoggedIn.js
@@ -23,7 +23,7 @@ export default function LoggedIn() {
           history.push("/");
         }}
       >
-        Logout
+        Log out
       </Button>
     </>
   );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,15 +5,31 @@ import Button from "react-bootstrap/Button";
 
 export default function Home() {
   return (
-    <Container
-      width="60%"
-      className="mt-3 justify-content-center align-text-center"
-    >
-      <Row className="row justify-content-center mt-3">
-        <Button href="/signup" variant="success">
-          Sign Up for Free
-        </Button>
-      </Row>
-    </Container>
+    <>
+      <Container
+        width="60%"
+        className="mt-3 justify-content-center align-text-center"
+      >
+        <Row className="row justify-content-center mt-3 mb-3">
+          <Button href="/signup" variant="success">
+            Sign Up for Free
+          </Button>
+        </Row>
+      </Container>
+      <Container
+        width="100%"
+        align="center"
+        className="mt-3 justify-content-center"
+      >
+        <iframe
+          width="80%"
+          height="500px"
+          src="https://www.youtube.com/embed/6P3Yol_9dDs"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </Container>
+    </>
   );
 }
